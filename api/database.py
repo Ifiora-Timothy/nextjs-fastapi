b86348fc4ee2de0df65cfb00d82c867fb7a6d560
Ifiora Timothy <ifioratimothy2001@gmail.com>
2025-01-04 11:16:44 +0100

    kk

---
+++ api/database.py
@@ -0,0 +1,8 @@
+
+
+class Names:
+    def __init__(self):
+        self.name="Timothy"
+
+    def get_names(self):
+        return self.name
--- api/index.py
+++ api/index.py
@@ -1,8 +1,13 @@
 from fastapi import FastAPI
-
+from database import Names
 ### Create FastAPI instance with custom docs and openapi url
 app = FastAPI(docs_url="/api/py/docs", openapi_url="/api/py/openapi.json")
 
 @app.get("/api/py/helloFastApi")
 def hello_fast_api():
     return {"message": "Hello from FastAPI"}
+
+@app.get("/api/py/getNames")
+def get_names():
+    names = Names()
+    return {"names": names.get_names()}
