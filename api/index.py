2ad83745b8f9b075f53625e8695cdb5438493130
Ifiora Timothy <ifioratimothy2001@gmail.com>
2025-01-04 11:19:34 +0100

    kk

--- api/index.py
+++ api/index.py
@@ -1,5 +1,5 @@
 from fastapi import FastAPI
-from database import Names
+from .database import Names
 ### Create FastAPI instance with custom docs and openapi url
 app = FastAPI(docs_url="/api/py/docs", openapi_url="/api/py/openapi.json")
 
